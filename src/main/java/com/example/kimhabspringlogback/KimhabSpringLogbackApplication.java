fba579ca329abfe963eb54d91ba2cf8cd83bacde
kimhab11 <keomhab@gmail.com>
2024-01-09 17:13:08 +0700

    git

---
+++ src/main/java/com/example/kimhabspringlogback/KimhabSpringLogbackApplication.java
@@ -0,0 +1,13 @@
+package com.example.kimhabspringlogback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KimhabSpringLogbackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KimhabSpringLogbackApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/kimhabspringlogback/LoggController.java
@@ -0,0 +1,28 @@
+package com.example.kimhabspringlogback;
+
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.PostConstruct;
+
+//@Slf4j
+@RestController
+public class LoggController {
+    Logger log = LoggerFactory.getLogger(LoggController.class);
+ //   @PostConstruct
+    @GetMapping()
+    public String index() {
+        log.trace("A TRACE Message");
+        log.debug("A DEBUG Message");
+        log.info("An INFO Message");
+        log.warn("A WARN Message");
+        log.error("An ERROR Message");
+
+        return "Howdy! Check out the Logs to see the output...";
+    }
+}
